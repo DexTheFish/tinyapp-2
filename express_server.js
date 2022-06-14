7a97cdb986d209918315628ff6921123b37e02f4
Declan Davis <dexthefish@gmail.com>
2022-06-14 01:04:02 -0400

    implemented function generateRandomString

--- express_server.js
+++ express_server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const shortURLLength = 6;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+  const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const upperAlphabet = lowerAlphabet.toUpperCase();
+  const alphaNumericChars = lowerAlphabet + upperAlphabet + '0123456789';
+  let randomString = '';
+  for (let i = 0; i < shortURLLength; i++) {
+    let randomIndex = Math.floor(Math.random() * 62);
+    randomString += alphaNumericChars[randomIndex]; 
+  }
+  return randomString;
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +31,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (req, res) => {
