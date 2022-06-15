f34b6275e52d8706cde93603f58441cbae28f3d4
Declan Davis <dexthefish@gmail.com>
2022-06-15 17:53:26 -0400

    finalised basic login/logout feature; preparing to merge with main.

--- express_server.js
+++ express_server.js
@@ -44,7 +44,6 @@ app.post("/urls", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
-  console.log('cookiessss');
   res.redirect("/urls");
 })
 
