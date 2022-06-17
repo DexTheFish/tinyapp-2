9a328416f04f7a3d81edbd43e3a437b8a64e8b7e
Declan Davis <dexthefish@gmail.com>
2022-06-17 16:05:30 -0400

    moved helper functions to separate file helpers.js. Installed mocha & chai. Created test folder.

--- express_server.js
+++ express_server.js
@@ -16,41 +16,42 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 
+const { generateRandomString, getUserByEmail, isNewEmail, urlsForUser } = require("./helpers");
 
-const generateRandomString = function(length) {
-  const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const upperAlphabet = lowerAlphabet.toUpperCase();
-  const alphaNumericChars = lowerAlphabet + upperAlphabet + '0123456789';
-  let randomString = '';
-  for (let i = 0; i < length; i++) {
-    let randomIndex = Math.floor(Math.random() * 62);
-    randomString += alphaNumericChars[randomIndex];
-  }
-  return randomString;
-};
+// const generateRandomString = function(length) {
+//   const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+//   const upperAlphabet = lowerAlphabet.toUpperCase();
+//   const alphaNumericChars = lowerAlphabet + upperAlphabet + '0123456789';
+//   let randomString = '';
+//   for (let i = 0; i < length; i++) {
+//     let randomIndex = Math.floor(Math.random() * 62);
+//     randomString += alphaNumericChars[randomIndex];
+//   }
+//   return randomString;
+// };
 
-const getUserByEmail = function(email, userDatabase) {
-  for (const userID in userDatabase) {
-    if (userDatabase[userID].email.toUpperCase() === email.toUpperCase()) {
-      return userDatabase[userID];
-    }
-  }
-  return null;
-};
+// const getUserByEmail = function(email, userDatabase) {
+//   for (const userID in userDatabase) {
+//     if (userDatabase[userID].email.toUpperCase() === email.toUpperCase()) {
+//       return userDatabase[userID];
+//     }
+//   }
+//   return null;
+// };
 
-const isNewEmail = function(email, userDatabase) {
-  return !Boolean(getUserByEmail(email, userDatabase));
-};
+// const isNewEmail = function(email, userDatabase) {
+//   return !Boolean(getUserByEmail(email, userDatabase));
+// };
 
-const urlsForUser = function(userID, urlDatabase) {
-  const userURLs = {};
-  for (let shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === userID) {
-      userURLs[shortURL] = urlDatabase[shortURL];
-    }
-  }
-  return userURLs;
-};
+// const urlsForUser = function(userID, urlDatabase) {
+//   const userURLs = {};
+//   for (let shortURL in urlDatabase) {
+//     if (urlDatabase[shortURL].userID === userID) {
+//       userURLs[shortURL] = urlDatabase[shortURL];
+//     }
+//   }
+//   return userURLs;
+// };
 
 const urlDatabase = {
   "b2xVn2": {
